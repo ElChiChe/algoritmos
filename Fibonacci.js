fbf9de066e45b5ec1d13b4ba91461def6ea7df77
Sebastian <69601416+ElChiChe@users.noreply.github.com>
2024-04-14 13:33:00 -0300

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,23 +1,23 @@
-// function fibs(n) {
+function fibs(n) {
 
-//     let arr = [0, 1];
+    let arr = [0, 1];
 
-//     for(let i = 2; i < n; i++) {
-//         arr[i] = arr[i - 1] + arr[i - 2];
-//     }
+    for(let i = 2; i < n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
 
-//     return arr;
-// }
+    return arr;
+}
 
-// console.log(fibs(8));
+console.log(fibs(8));
 
-// function recursiveFib(n) {
-//     if(n <= 1) {
-//         return n;
-//     }
-//     else {
-//         return recursiveFib(n - 1) + recursiveFib(n - 2);
-//     }
-// }
+function recursiveFib(n) {
+    if(n <= 1) {
+        return n;
+    }
+    else {
+        return recursiveFib(n - 1) + recursiveFib(n - 2);
+    }
+}
 
-// console.log(recursiveFib(8));
+console.log(recursiveFib(8));
